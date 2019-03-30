7010cdd0424d90ba95fd67c694bcb67bf3f5c4f8
Cédric David <cedric.david@btopenworld.com>
2019-03-30 15:28:32 +0800

    add TodoIem  component

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+import PropTypes from "prop-types";
 
 class Todos extends Component {
   render() {
-    return this.props.todos.map(todo => <h3>{todo.title}</h3>);
+    return this.props.todos.map(todo => (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        markComplete={this.props.markComplete}
+      />
+    ));
   }
 }
+
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
+};
 
 export default Todos;
